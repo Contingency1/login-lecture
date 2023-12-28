c93ec07c3681b3f91969abe281af1cfdb196f109
JYE <jo1814@naver.com>
2023-12-28 12:52:26 +0900

    로그인을 요청하는 fetch 생성

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.hello);
-
 router.get("/login", ctrl.login);
+// router.post("/login", ctrl.login);
+
 module.exports = router;
